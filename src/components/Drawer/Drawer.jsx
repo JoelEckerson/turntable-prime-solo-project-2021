c3ca4831f44485a7e833797d89af6ca5fff392b3
Joel Eckerson <joelaeckerson@gmail.com>
2021-04-21 17:09:32 -0500

    material ui drawer taking objects

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -5,19 +5,47 @@ import {
     List,
     ListItemIcon, 
     ListItemText 
-} from '@material-ui/core'
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    drawer: {
+        width: '160px'
+    }
+})
 
 function Drawer() {
+    const classes = useStyles();
+    const itemsList = [{
+            text: 'Home'
+        },
+        {
+            text: 'Wantlist'
+        },
+        {
+            text: 'Collection'
+        },
+        {
+            text: 'Search'
+        },
+        {
+            text: 'Add Record'
+        },
+        {
+            text: 'Recommendations'
+        }];
     return (
         <div>
-            <MUIDrawer variant="permanent">
+            <MUIDrawer variant="permanent" className = {classes.drawer}>
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                {itemsList.map((item, index) => {
+                const { text } = item;
+                return (
                 <ListItem button key={text}>
                     {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
                     <ListItemText primary={text} />
                 </ListItem>
-                ))}
+                )})}
             </List>
             </MUIDrawer>
         </div>
