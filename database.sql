e94506d874dfba0e962ec73c467cb8999671595d
Joel Eckerson <joelaeckerson@gmail.com>
2021-04-20 20:24:38 -0500

    album router GET x4

--- database.sql
+++ database.sql
@@ -6,5 +6,33 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "name" VARCHAR (50),
+    "email" VARCHAR (50),
+    "bio" VARCHAR (1000)
+);
+
+CREATE TABLE "album" (
+    "id" SERIAL PRIMARY KEY,
+    "artist" VARCHAR (80) UNIQUE NOT NULL,
+    "album" VARCHAR (1000) NOT NULL,
+    "genre" VARCHAR (50) NOT NULL,
+    "year" VARCHAR (50),
+    "image_url" VARCHAR (1000)
+);
+
+CREATE TABLE "wantlist" (
+    "id" SERIAL PRIMARY KEY,
+    "album_id" INT REFERENCES "album" NOT NULL,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "comments" VARCHAR (1000),
+    "rating" VARCHAR (10)
+);
+
+CREATE TABLE "collection" (
+    "id" SERIAL PRIMARY KEY,
+    "album_id" INT REFERENCES "album" NOT NULL,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "comments" VARCHAR (1000),
+    "rating" VARCHAR (10)
 );
---
+++ server/routes/album.router.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/artist', (req, res) =>{
+    const query = `SELECT * FROM "album" WHERE artist=$1`;
+    console.log('in album', req.params.artist);
+    pool.query(query, [req.params.artist])
+    .then( result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('ERROR: Get Album Artist', err);
+        res.sendStatus(500)
+    })
+});
+
+router.get('/album', (req, res) =>{
+    const query = `SELECT * FROM "album" WHERE album=$1`;
+    console.log('in album', req.params.album);
+    pool.query(query, [req.params.album])
+    .then( result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('ERROR: Get Album Album', err);
+        res.sendStatus(500)
+    })
+});
+
+router.get('/genre', (req, res) =>{
+    const query = `SELECT * FROM "album" WHERE genre=$1`;
+    console.log('in album', req.params.genre);
+    pool.query(query, [req.params.genre])
+    .then( result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('ERROR: Get Album Genre', err);
+        res.sendStatus(500)
+    })
+});
+
+router.get('/year', (req, res) =>{
+    const query = `SELECT * FROM "album" WHERE year=$1`;
+    console.log('in album', req.params.year);
+    pool.query(query, [req.params.year])
+    .then( result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('ERROR: Get Album Year', err);
+        res.sendStatus(500)
+    })
+});
+
+router.get('/', (req, res) =>{
+    const query = `SELECT * FROM "album"`;
+    console.log('in album');
+    pool.query(query)
+    .then( result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('ERROR: Get Album Artist', err);
+        res.sendStatus(500)
+    })
+});
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
