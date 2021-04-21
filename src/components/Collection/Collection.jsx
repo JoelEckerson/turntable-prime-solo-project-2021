1d46e91d6c8d08909cefa60aaefbba0fade7c507
Joel Eckerson <joelaeckerson@gmail.com>
2021-04-21 18:20:37 -0500

    drawer working but permanent

---
+++ src/components/Collection/Collection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Collection() {
+    return (
+        <div>
+            This is the Collection
+        </div>
+    )
+}
+
+export default Collection
--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -8,41 +8,55 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+
+
+// changes the width of the Material UI drawer
 const useStyles = makeStyles({
     drawer: {
-        width: '160px'
+        width: '190px'
     }
 })
 
 function Drawer() {
     const classes = useStyles();
+    // this variable with hold an array of objects to be mapped through for the drawer
     const itemsList = [{
-            text: 'Home'
+            text: 'Home',
+            
         },
         {
-            text: 'Wantlist'
+            text: 'Wantlist',
+            
         },
         {
-            text: 'Collection'
+            text: 'Collection',
+            
         },
         {
-            text: 'Search'
+            text: 'Search',
+            
         },
         {
-            text: 'Add Record'
+            text: 'Add Record',
+            
         },
         {
-            text: 'Recommendations'
+            text: 'Recommendations',
+            
+        },
+        {
+            text: 'LogOut',
         }];
     return (
         <div>
+            {/* this creates the Material UI drawer */}
             <MUIDrawer variant="permanent" className = {classes.drawer}>
             <List>
                 {itemsList.map((item, index) => {
-                const { text } = item;
+                const { text, icon } = item;
                 return (
                 <ListItem button key={text}>
-                    {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
+                    {/* {icon && <ListItemIcon>{icon}</ListItemIcon>} */}
                     <ListItemText primary={text} />
                 </ListItem>
                 )})}
