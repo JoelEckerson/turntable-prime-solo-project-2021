ea340b08b4f321e122c65c14224bb851ed8ffc13
Joel Eckerson <joelaeckerson@gmail.com>
2021-04-24 16:00:35 -0500

    drawer working fully

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { 
     Drawer as MUIDrawer, 
+    Button,
     ListItem, 
     List,
     ListItemIcon, 
@@ -8,6 +9,7 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import {withRouter} from 'react-router-dom';
+import { useState } from 'react';
 
 
 
@@ -21,24 +23,33 @@ const useStyles = makeStyles({
 
 
 function Drawer(props) {
+
+
+
     const {history} = props;
     const classes = useStyles();
+    const [showDrawer, setShowDrawer] = useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+        setShowDrawer({open})
+    }
+
     // this variable with hold an array of objects to be mapped through for the drawer
     const itemsList = [{
             text: 'Home',
-            onClick: () => history.push("/")
+            onClick: () => {history.push("/"), setShowDrawer(false)}
         },
         {
             text: 'Wantlist',
-            onClick: () => history.push("/wantlist")
+            onClick: () => {history.push("/wantlist"); setShowDrawer(false)}
         },
         {
             text: 'Collection',
-            onClick: () => history.push("/collection")
+            onClick: () => {history.push("/collection"); setShowDrawer(false)}
         },
         {
             text: 'Search',
-            onClick: () => history.push("/search")
+            onClick: () => {history.push("/search"); setShowDrawer(false)}
         },
         {
             text: 'Recommendations',
@@ -50,7 +61,8 @@ function Drawer(props) {
     return (
         <div>
             {/* this creates the Material UI drawer */}
-            <MUIDrawer variant="permanent" className = {classes.drawer}>
+            <Button onClick={toggleDrawer(true)}>...</Button>
+            <MUIDrawer open={showDrawer} className = {classes.drawer}>
             <List>
                 {itemsList.map((item, index) => {
                 const { text, icon, onClick } = item;
