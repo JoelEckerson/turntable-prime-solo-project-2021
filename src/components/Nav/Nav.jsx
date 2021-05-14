396cad795dc131d6030707aba89c06027135d9df
Joel Eckerson <joelaeckerson@gmail.com>
2021-05-14 09:58:39 -0500

    app bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -152,35 +152,6 @@ function Nav() {
                     <ChatOutlinedIcon />
                   </IconButton>
                   </Grid>
-                    {/* <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircleOutlinedIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div> */}
                 </Toolbar>
             </AppBar>
             <div onKeyDown={toggleDrawer(false)}>
